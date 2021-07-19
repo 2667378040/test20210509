6496a2cd9ff8e18a82ae0fe676be10e2567719fb
chenyu <2667378040@qq.com>
2021-07-19 16:14:59 +0800

    develop 第一次提交

--- src/main/java/com/chenyu/test20210509/AClass.java
+++ src/main/java/com/chenyu/test20210509/AClass.java
@@ -5,4 +5,8 @@ public class AClass {
     public void methodOne(){
         System.out.println("methodOne 123");
     }
+
+    public void methodThree() {
+        System.out.println("methodThree");
+    }
 }
